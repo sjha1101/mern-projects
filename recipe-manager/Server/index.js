83c16ea498b24438df6ab784b7c53ca1ccfd9d90
Surbhi Jha <codingchannel8i@gmail.com>
2025-12-09 22:51:21 +0530

    index file updated

--- recipe-manager/Server/index.js
+++ recipe-manager/Server/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
@@ -12,25 +11,17 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://mern-projects-ao1kgvluv-surbhi-jhas-projects.vercel.app",
+    origin: ["https://mern-projects-ao1kgvluv-surbhi-jhas-projects.vercel.app"],
     credentials: true,
   })
 );
 
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("MongoDB Error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Error:", err));
 
 app.use("/api", userRoutes);
 app.use("/api", recipeRoutes);
-
-app.use(express.static(path.join(__dirname, "../Client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Client/dist/index.html"));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
